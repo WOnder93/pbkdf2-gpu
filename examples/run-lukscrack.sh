c0308cd588e35466b766b3f2ff0325225031f14d
Ondrej Mosnáček <omosnacek@gmail.com>
2016-02-02 13:56:21 +0100

    Removed '--opencl-data-dir=...' from the example script.

--- examples/run-lukscrack.sh
+++ examples/run-lukscrack.sh
@@ -5,4 +5,4 @@ PWLIST=$2
 BATCHSIZE=$3
 HEADER=$4
 
-"$1" --opencl-data-dir=../libpbkdf2-compute-opencl/data --pwgen=list:"$PWLIST" -b $BATCHSIZE "$HEADER"
+"$1" --pwgen=list:"$PWLIST" -b $BATCHSIZE "$HEADER"
